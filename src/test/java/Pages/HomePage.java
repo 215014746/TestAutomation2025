e57dd0afd35de4839dfcff365a564c7401698214
Zinhle Ndubane <ndubanepz@gmail.com>
2025-10-28 15:45:33 +0200

    created home page and verifyHopePageIsDisplayed method

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+   WebDriver driver;
+
+   @FindBy(id = "overview-hero")
+    WebElement homepageTitle_id;
+
+   public HomePage(WebDriver driver){
+       this.driver = driver;
+   }
+
+   public void verifyHopePageIsDisplayed(){
+       homepageTitle_id.isDisplayed();
+   }
+}
